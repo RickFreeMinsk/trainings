6ad219b2ad141e334585c7c1903a9757595be3ab
David Shuster <david.shuster@itechart-group.com>
2017-04-21 18:21:25 +0300

    add mobile responsive styling

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-(function() {
-    var element =  document.querySelectorAll('.date'),
+(function () {
+    var element = document.querySelectorAll('.date'),
         parent = element[0].parentElement;
+
+    function toggleHidden() {
+        element[0].classList.toggle('-hidden');
+    }
+
     if (parent.classList.contains('box-plus')) {
-        parent.addEventListener('click', function() {
-           element[0].classList.toggle('-hidden');
+        parent.addEventListener('click', function (event) {
+            event.stopPropagation();
+            toggleHidden();
+            document.querySelector('body').addEventListener('click', function bodyClick() {
+                toggleHidden();
+                this.removeEventListener('click', bodyClick);
+            });
         });
     }
 })();
