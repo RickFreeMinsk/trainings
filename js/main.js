cc564ac81c0b10de29fda72b5dabe2da4a5fa660
David Shuster <david.shuster@itechart-group.com>
2017-04-24 09:53:36 +0300

    add module

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
-var element = document.querySelectorAll('.date'),
-    parent = element[0].parentElement;
+(function () {
+    var element = document.querySelectorAll('.date'),
+        parent = element[0].parentElement;
 
-function toggleHidden() {
-    element[0].classList.toggle('-hidden');
-}
+    function toggleHidden() {
+        element[0].classList.toggle('-hidden');
+    }
 
-if (parent.classList.contains('box-plus')) {
-    parent.addEventListener('click', function (event) {
-        event.stopPropagation();
-        toggleHidden();
-        document.querySelector('body').addEventListener('click', function bodyClick() {
+    if (parent.classList.contains('box-plus')) {
+        parent.addEventListener('click', function (event) {
+            event.stopPropagation();
             toggleHidden();
-            this.removeEventListener('click', bodyClick);
+            document.querySelector('body').addEventListener('click', function bodyClick() {
+                toggleHidden();
+                this.removeEventListener('click', bodyClick);
+            });
         });
-    });
-}
+    }
+})();
